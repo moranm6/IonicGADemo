99940b23d35e19cd33de95a322b597acf6db08af
Jared Faris <jfaris@gmail.com>
2015-06-18 13:54:13 -0400

    Added a stubbed joke generator for others to fill in

---
+++ ScoreboardServer/Services/JokeGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI.WebControls.WebParts;
+using ScoreboardServer.Models;
+
+namespace ScoreboardServer.Services
+{
+    public class StubJokeGenerator : IJokeGenerator
+    {
+        public string GenerateJoke(Player submittingPlayer, string submittedText)
+        {
+            return String.Format("{0} {1} says {2}", submittingPlayer.FirstName, submittingPlayer.LastName, submittedText);
+
+        }
+    }
+}
